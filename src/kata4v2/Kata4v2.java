27bee5bf1e430d821e89a5e129c5a3218c528fea
usuario <usuario@10.22.143.13>
2015-10-27 09:28:24 +0000

    kata 4 primera versión

---
+++ src/kata4v2/Kata4v2.java
@@ -0,0 +1,26 @@
+package kata4v2;
+
+import java.io.File;
+
+public class Kata4v2 {
+
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\usuario\\Desktop");
+        print(file.listFiles());
+    }
+
+    private static void print(File[] files) {
+        print(files, "");
+    }
+
+    private static void print(File[] files, String indent) {
+        if(files == null) return;
+            
+        for (File file : files){
+            System.out.println(indent + (file.isDirectory()? "+" : " ") + file.getName());
+            if(!file.isDirectory() || file.isHidden()) continue;
+            print (file.listFiles(), indent + "  ");
+        }
+        
+    }
+}
